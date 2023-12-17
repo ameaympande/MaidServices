96d02279e0629a514bd0dad7f7ee966ad9540c0e
Ameay Pande <ameay35@gmail.com>
2023-12-17 13:25:23 +0530

    added our service section on homepage

--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -10,12 +10,36 @@ import {
   Textarea,
   Checkbox,
 } from "@material-tailwind/react";
-import { FingerPrintIcon, UsersIcon } from "@heroicons/react/24/solid";
 import { PageTitle, Footer } from "@/widgets/layout";
 import { FeatureCard, TeamCard } from "@/widgets/cards";
 import { featuresData, teamData, contactData } from "@/data";
+import CustomCard from "@/widgets/cards/CustomCard";
 
 export function Home() {
+  const cardData = [
+    {
+      imageSrc: "/img/cookingMaid.jpg",
+      title: "Cooks",
+      rating: 4,
+    },
+    {
+      imageSrc: "/img/cleaningMaid.jpg",
+      title: "Clean",
+      rating: 5,
+    },
+    {
+      imageSrc: "/img/allrounderMaid.png",
+      title: "All rounders",
+      rating: 4.5,
+    },
+    {
+      imageSrc: "/img/babysitMaid.jpg",
+      title: "Babysitter / Japa",
+      rating: 4.5,
+    },
+
+    // Add more data as needed
+  ];
   return (
     <>
       <div className="relative flex h-screen content-center items-center justify-center pt-16 pb-32">
@@ -41,7 +65,7 @@ export function Home() {
           </div>
         </div>
       </div>
-      <section className="-mt-32 bg-white px-4 pb-20 pt-4 bg-trasnparent">
+      <section className="-mt-32 bg-white px-4 pb-20 pt-4 bg-transparent">
         <div className="container mx-auto">
           <div className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-4 ">
             {featuresData.map(({ image, title, description }) => (
@@ -53,61 +77,19 @@ export function Home() {
               />
             ))}
           </div>
-          <div className="mt-32 flex flex-wrap items-center">
-            <div className="mx-auto -mt-8 w-full px-4 md:w-5/12">
-              <div className="mb-6 inline-flex h-16 w-16 items-center justify-center rounded-full bg-blue-gray-900 p-2 text-center shadow-lg">
-                <FingerPrintIcon className="h-8 w-8 text-white " />
-              </div>
-              <Typography
-                variant="h3"
-                className="mb-3 font-bold"
-                color="blue-gray"
-              >
-                Working with us is a pleasure
+          <div className="mt-32">
+            <div className="mx-auto w-full px-4 md:w-5/12 text-center">
+              <Typography variant="h6" className="mb-3 font-bold" color="blue">
+                Our Featured Service
               </Typography>
-              <Typography className="mb-8 font-normal text-blue-gray-500">
-                Don't let your uses guess by attaching tooltips and popoves to
-                any element. Just make sure you enable them first via
-                JavaScript.
-                <br />
-                <br />
-                The kit comes with three pre-built pages to help you get started
-                faster. You can change the text and images and you're good to
-                go. Just make sure you enable them first via JavaScript.
+              <Typography variant="h2" className="mt-4 mb-8 font-bold">
+                Hire Professionals, Experienced Specifically for Your Needs
               </Typography>
-              <Button variant="filled">read more</Button>
             </div>
-            <div className="mx-auto mt-24 flex w-full justify-center px-4 md:w-4/12 lg:mt-0">
-              <Card className="shadow-lg border shadow-gray-500/10 rounded-lg">
-                <CardHeader floated={false} className="relative h-56">
-                  <img
-                    alt="Card Image"
-                    src="/img/teamwork.png"
-                    className="h-full w-full"
-                  />
-                </CardHeader>
-                <CardBody>
-                  <Typography
-                    variant="small"
-                    color="blue-gray"
-                    className="font-normal"
-                  >
-                    Enterprise
-                  </Typography>
-                  <Typography
-                    variant="h5"
-                    color="blue-gray"
-                    className="mb-3 mt-2 font-bold"
-                  >
-                    Top Notch Services
-                  </Typography>
-                  <Typography className="font-normal text-blue-gray-500">
-                    The Arctic Ocean freezes every winter and much of the
-                    sea-ice then thaws every summer, and that process will
-                    continue whatever happens.
-                  </Typography>
-                </CardBody>
-              </Card>
+            <div className="mx-auto mt-24 flex flex-wrap justify-center gap-20">
+              {cardData.map((card, index) => (
+                <CustomCard key={index} {...card} />
+              ))}
             </div>
           </div>
         </div>
--- frontend/src/widgets/cards/feature-card.jsx
+++ frontend/src/widgets/cards/feature-card.jsx
@@ -8,8 +8,8 @@ import {
 
 export function FeatureCard({ image, title, description }) {
   return (
-    <Card className="rounded-lg shadow-lg shadow-gray-500/10 bg-amber-200">
-      <CardBody className="px-8 text-center ">
+    <Card className="rounded-lg shadow-lg shadow-gray-500/10 bg-amber-200 transform hover:scale-105 transition-transform">
+      <CardBody className="px-8 text-center">
         <div>
           <img src={image} alt={title} className="w-25 h-18 rounded-full" />
         </div>
