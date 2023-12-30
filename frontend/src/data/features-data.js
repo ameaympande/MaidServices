f454ed9533ba0ed6103eb90cc63c05d18174615e
Ameay Pande <ameay35@gmail.com>
2023-12-30 08:18:12 +0530

    added img URI path

--- frontend/src/data/features-data.js
+++ frontend/src/data/features-data.js
@@ -1,33 +1,31 @@
-import cook from "../../public/img/cook.jpg";
-import clean from "../../public/img/clean.jpg";
-import babysit from "../../public/img/babysit.avif";
-import allround from "../../public/img/allrounder.png";
 import { MessageCircle } from "lucide-react";
+
+const APP_URI = "https://total-home.onrender.com";
 
 export const featuresData = [
   {
-    image: cook,
+    image: `${APP_URI}/img/cook.jpg`,
     title: "Cooks",
     icon: MessageCircle,
     description:
-      "Choose between basic home-style or fancy cooking at affortable rates!",
+      "Choose between basic home-style or fancy cooking at affordable rates!",
   },
   {
-    image: clean,
+    image: `${APP_URI}/img/clean.jpg`,
     title: "Clean",
     icon: MessageCircle,
     description:
-      "Keep your home clean at anytime so you don't need to bother at your schedule!",
+      "Keep your home clean at any time so you don't need to bother with your schedule!",
   },
   {
-    image: babysit,
+    image: `${APP_URI}/img/babysit.avif`,
     title: "Babysitters",
     icon: MessageCircle,
     description:
       "Experienced babysitters and japa for all your child-care needs!",
   },
   {
-    image: allround,
+    image: `${APP_URI}/img/allrounder.png`,
     title: "All-Rounders",
     icon: MessageCircle,
     description:
